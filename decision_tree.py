e1dc694ade7e8a8f16b1cf9652d8605b84ac2203
Stacy <stacy.kaing@mines-ales.org>
2023-03-17 15:53:33 +0100

    main

--- decision_tree.py
+++ decision_tree.py
@@ -2,32 +2,31 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 #Tableau X contenant les données d'entrainement et Y contenant les etiquettes des classes (70%)
-X = np.array([ [1, 0, 0, 1, 1, 0, 0, 1, 0, 0], # première équipe
+X_train = np.array([ [1, 0, 0, 1, 1, 0, 0, 1, 0, 0], # première équipe
                [0, 1, 0, 0, 0, 1, 1, 0, 1, 0], # deuxième équipe
              ])
-Y = np.array([1, 0, 1, 0, 1, 1, 0, 0, 1, 0, ...]) #résultats des matchs
+y_train = np.array([1, 0, 1, 0, 1, 1, 0, 0, 1, 0, ...]) #résultats des matchs
 
 #Tableau X_test et Y_test contenant les données de test (30%)
 X_test =np.array([ [1, 0, 0, 1, 1, 0, 0, 1, 0, 0], # première équipe
                [0, 1, 0, 0, 0, 1, 1, 0, 1, 0], # deuxième équipe
              ])
-Y_test = np.array([1, 0, 1, 0, 1, 1, 0, 0, 1, 0, ...]) #résultats des matchs
+y_test = np.array([1, 0, 1, 0, 1, 1, 0, 0, 1, 0, ...]) #résultats des matchs
 #DECISION TREE
 #Crée une instance de la classe DecisionTreeClassifier en spécifiant les paramètres de l'algorithme: le critère de séparation des nœuds (entropy) et la profondeur maximale de l'arbre.
 clf = DecisionTreeClassifier(criterion='entropy', max_depth=3)
 
 #On entraîne l'arbre de décision sur les données d'apprentissage
-clf.fit(X, Y)
-
-#On évalue les performances de l'arbre de décision sur les données d'entraînement
-train_score = clf.score(X, Y)
-print("Score de l'arbre de décision sur les données d'entraînement : {:.2f}".format(train_score))
+clf.fit(X_train, y_train)
+print(X_train.shape)
 
 #On prédit les résultats des matchs sur les données de test
 Y_pred = clf.predict(X_test)
 
-#On évalue les performances de l'arbre de décision sur les données de test
-test_score = clf.score(X_test, Y_test)
+#On évalue les performances du modèle sur les données d'entraînement et de test
+train_score = clf.score(X_train, y_train)
+print("Score de l'arbre de décision sur les données d'entraînement : {:.2f}".format(train_score))
+test_score = clf.score(X_test, y_test)
 print("Score de l'arbre de décision sur les données de test : {:.2f}".format(test_score))
 
 
